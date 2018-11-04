2bb249d683950127e974ea6cbb2b601573242cb6
Akc52 <a_lys_sa@hotmail.com>
2018-11-04 17:12:00 -0500

    Add routes

--- lessons/week-2/example3/index.js
+++ lessons/week-2/example3/index.js
@@ -1,24 +1,4 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-
-const app = express()
+const app = require('./api/server')
 const PORT = 3001
 
-app.use(bodyParser.json())
-
-app.get('/', (request, response) => {
-  response.send("Hi World!")
-})
-
-app.get('/name', (request, response) => {
-  response.send('Alyssa Ciccone')
-})
-
-app.get('/mailbox', (req, res) => {
-  res.json({
-    firstName: 'Alyssa',
-    lastName: 'Ciccone'
-  })
-})
-
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`App listening on ${PORT}`))
