492132e37a81933a69ccbcde78ab96f4deddd676
Akc52 <a_lys_sa@hotmail.com>
2018-11-25 13:52:09 -0500

    Add seeds

--- lessons/week-5/blog/index.js
+++ lessons/week-5/blog/index.js
@@ -1,6 +1,12 @@
 'use strict';
 
+const mongoose = require('mongoose')
+
 const server = require('./api/server');
 const { PORT } = require('./api/utils/constants');
 
-server.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+server.listen(PORT, async () => {
+  await mongoose.connect(DB_URI)
+  console.log(`Database connected at ${DB_URI}`)
+  console.log(`App listening on port ${PORT}`)
+});
